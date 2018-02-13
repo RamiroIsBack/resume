500e9e76a70fabf3f456bb8aa391d5433e85ffbe
Ramiro <ramiro.santamaria.alvarez@gmail.com>
2018-02-13 17:25:15 -0500

    readjust infowindow on map

--- src/components/presentational/Mapa.js
+++ src/components/presentational/Mapa.js
@@ -26,16 +26,16 @@ class Mapa extends Component {
       >
 
         <InfoWindow >
-          <div style = {{maxWidth: 600,padding : 0}}>
+          <div style = {{maxWidth: 600,padding : 0, margin: 0}}>
 
             <img role='presentation' src={foto} className ='infoWindow__img' alt='foto' id='fotoInfoWindow'
               style = {{
-                opacity: 0.9, maxHeight: 60, maxWidth:60, cursor: 'pointer',float:'left', marginRight:'2px',
+                opacity: 0.9, maxHeight: 60, maxWidth:60, cursor: 'pointer', marginRight:'2px',
               }}
               onClick={() => this.handleClick('https://www.google.com/maps/place/Hampden,+Baltimore,+Maryland+21211/@39.3399061,-76.6020314,12.93z/data=!4m5!3m4!1s0x89c804d5f7905ff3:0x5cc1e5ec25566796!8m2!3d39.3333706!4d-76.632607')}>
             </img>
 
-            <div className='infoWindow__bio' style = {{padding : 0,float:'right'}} >
+            <div className='infoWindow__bio' style = {{padding : 0,}} >
               <h6 style = {{marginTop : 2,marginBottom:2}}>I'm in</h6>
               <h5 style = {{marginTop : 2,marginBottom:0}}>Hampden</h5>
               <h6 style = {{marginTop : 2,marginBottom:2}}>Baltimore, MD</h6>
@@ -66,7 +66,7 @@ class Mapa extends Component {
       <GoogleMap
 
         defaultZoom={9}
-        defaultCenter={{  lat: 39.4832391, lng: -76.660066  }}
+        defaultCenter={{  lat: 39.5332391, lng: -76.610066  }}
       >
         {marker}
       </GoogleMap>
