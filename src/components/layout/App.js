eafe9bc925a27218ac78190b7be02d113835bc3e
Ramiro <ramiro.santamaria.alvarez@gmail.com>
2018-03-09 19:29:31 -0500

    getting rid of warnings in the console3

--- src/components/layout/App.js
+++ src/components/layout/App.js
@@ -83,7 +83,7 @@ class App extends Component {
         <NombreContainer />
 
 
-        <div className='Main__container'>
+        <div className='main__container'>
           <div className='IntroContainer'>
             <IntroContainer  />
 
