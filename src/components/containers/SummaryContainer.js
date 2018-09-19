6ae8e707abc0fd2bebc609938842e7c5ce7b8c6a
Ramiro <ramiro.santamaria.alvarez@gmail.com>
2018-09-19 19:11:09 -0400

    fixing scroll indicator starting too soon now for real making it wait until a function is called at the end of the stack

--- src/components/containers/SummaryContainer.js
+++ src/components/containers/SummaryContainer.js
@@ -1,113 +1,133 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import actions from '../../actions';
-import {PopularFunctions} from '../../utils';
-import {Summary_css} from '../../css'; // eslint-disable-line no-unused-vars
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import actions from "../../actions";
+import { PopularFunctions } from "../../utils";
+import { Summary_css } from "../../css"; // eslint-disable-line no-unused-vars
 
 class SummaryContainer extends Component {
-
-  handleImageLoaded(){
-
-    this.props.imgLoaded(true);
+  handleImageLoaded() {
+    setTimeout(() => {
+      //this function is called when everything else is done,
+      // this way we ensure that is not caching false scroll inputs
+      this.props.imgLoaded(true);
+    }, 0);
   }
-  handleImageErrored(){
-
+  handleImageErrored() {
     this.props.imgLoaded(false);
   }
-  handleClick(){
-
-    this.props.movetoSection('footer');
-    const element = document.getElementById('footerContainer');
-    element.scrollIntoView({block: 'start',  behaviour: 'smooth'});
+  handleClick() {
+    this.props.movetoSection("footer");
+    const element = document.getElementById("footerContainer");
+    element.scrollIntoView({ block: "start", behaviour: "smooth" });
   }
 
   render() {
-    let urlPic = '';
-    let bio = '';
+    let urlPic = "";
+    let bio = "";
 
-    let copy = PopularFunctions.selectSpecificCopy(this.props,'summaryPic');
+    let copy = PopularFunctions.selectSpecificCopy(this.props, "summaryPic");
 
-    if (copy.bio){
+    if (copy.bio) {
       urlPic = copy.urlPic;
       bio = copy.bio;
     }
     let tablet = false;
-    if (this.props.section.screenSize ==='tablet' || this.props.section.screenSize ==='mobile'){
+    if (
+      this.props.section.screenSize === "tablet" ||
+      this.props.section.screenSize === "mobile"
+    ) {
       tablet = true;
     }
 
     return (
       <div>
-        <div className = 'summary__container'>
-          {!tablet &&
+        <div className="summary__container">
+          {!tablet && (
             <div>
-              <div className = 'summary__photo__container' >
-                <div className = 'summary__copy'>
-                  <h2 className = 'head__line'>
-                    About me:
-                  </h2>
-                  <div className = 'bio__container'>
-                    {bio.split('\n').map((item, key) => {
-                      return <span className = 'bio' key={key}>{item}<div><br/></div></span>;})}
+              <div className="summary__photo__container">
+                <div className="summary__copy">
+                  <h2 className="head__line">About me:</h2>
+                  <div className="bio__container">
+                    {bio.split("\n").map((item, key) => {
+                      return (
+                        <span className="bio" key={key}>
+                          {item}
+                          <div>
+                            <br />
+                          </div>
+                        </span>
+                      );
+                    })}
                   </div>
                 </div>
                 <div>
-                  
-                  <img className = 'summary__photo' src = {urlPic}
-                    alt=''
+                  <img
+                    className="summary__photo"
+                    src={urlPic}
+                    alt=""
                     onLoad={this.handleImageLoaded.bind(this)}
                     onError={this.handleImageErrored.bind(this)}
-                  ></img>
+                  />
                 </div>
               </div>
             </div>
-          }
-          {tablet &&
+          )}
+          {tablet && (
             <div>
-              <div className = 'headLine__andPhoto__container'>
-                <h2 className = 'head__line'>
-                  Some words about me:
-                </h2>
-                <h2 className = 'contact__button'
-                  onClick = {this.handleClick.bind(this)}
+              <div className="headLine__andPhoto__container">
+                <h2 className="head__line">Some words about me:</h2>
+                <h2
+                  className="contact__button"
+                  onClick={this.handleClick.bind(this)}
                 >
                   Contact info
                 </h2>
-                <div className = 'summary__photo__container' >
-                  <img className = 'summary__photo' src = {urlPic}
-                    alt=''
+                <div className="summary__photo__container">
+                  <img
+                    className="summary__photo"
+                    src={urlPic}
+                    alt=""
                     onLoad={this.handleImageLoaded.bind(this)}
                     onError={this.handleImageErrored.bind(this)}
-                  ></img>
+                  />
                 </div>
               </div>
-              <div className = 'summary__copy'>
-                <div className = 'bio__container'>
-                  {bio.split('\n').map((item, key) => {
-                    return <span className = 'bio' key={key}>{item}<br/><br/></span>;})}
+              <div className="summary__copy">
+                <div className="bio__container">
+                  {bio.split("\n").map((item, key) => {
+                    return (
+                      <span className="bio" key={key}>
+                        {item}
+                        <br />
+                        <br />
+                      </span>
+                    );
+                  })}
                 </div>
               </div>
             </div>
-          }
+          )}
         </div>
       </div>
     );
   }
 }
 
-const dispatchToProps = (dispatch) =>{
-
-  return{
-    movetoSection: (section) => dispatch(actions.movetoSection(section)),
+const dispatchToProps = dispatch => {
+  return {
+    movetoSection: section => dispatch(actions.movetoSection(section)),
     getCopy: () => dispatch(actions.getCopy()),
-    imgLoaded: (loaded) => dispatch (actions.imgLoaded(loaded)),
+    imgLoaded: loaded => dispatch(actions.imgLoaded(loaded))
   };
 };
-const stateToProps = (state) => {
-  return{
-    copy :state.copy,
-    section: state.section,
+const stateToProps = state => {
+  return {
+    copy: state.copy,
+    section: state.section
   };
 };
 
-export default connect (stateToProps,dispatchToProps)(SummaryContainer);
+export default connect(
+  stateToProps,
+  dispatchToProps
+)(SummaryContainer);
