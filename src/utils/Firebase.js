98150a7c0c2f061cb188595cbab23c8f4bf0c96c
ra <ramiro.santamaria.alvarez@gmail.com>
2019-04-04 16:46:25 -0400

    edited intro foto margin and background color, also updating firebase imports

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -1,45 +1,46 @@
-import firebase from 'firebase';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+import { firebaseConfig } from "./FirebaseConfig";
 
-var config = {
-  apiKey: 'AIzaSyDF1G-uxr_Sg4jHLZAgyuVXsuHnTt8bhzU',
-  authDomain: 'resume-40a8a.firebaseapp.com',
-  databaseURL: 'https://resume-40a8a.firebaseio.com',
-  projectId: 'resume-40a8a',
-  storageBucket: 'resume-40a8a.appspot.com',
-  messagingSenderId: '714638486475'
-};
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 let copyList = [];
 
 const getCopy = (params, actionType) => {
-  return dispatch => database.ref('copy').once('value')
-    .then(snapshot => {
-      snapshot.forEach(function (childSnapshot) {
-        const valor = childSnapshot.val();
-        valor.id = childSnapshot.key;
-        copyList.push(valor);
-      });
-      if (actionType != null) {
-        dispatch({
-          type: actionType,
-          params: params, // can be null
-          data: copyList, // list with all d objects
+  return dispatch =>
+    database
+      .ref("copy")
+      .once("value")
+      .then(snapshot => {
+        snapshot.forEach(function(childSnapshot) {
+          const valor = childSnapshot.val();
+          valor.id = childSnapshot.key;
+          copyList.push(valor);
         });
-      }
+        if (actionType != null) {
+          dispatch({
+            type: actionType,
+            params: params, // can be null
+            data: copyList // list with all d objects
+          });
+        }
 
-      return copyList;
-    })
-    .catch(err => {
-      console.log('pics in db didnt download correctly');
-      throw err;
-    });
+        return copyList;
+      })
+      .catch(err => {
+        console.log("pics in db didnt download correctly");
+        throw err;
+      });
 };
 const getCopyForTesting = () => {
   return new Promise((resolve, reject) => {
-    database.ref('copy').once('value')
+    database
+      .ref("copy")
+      .once("value")
       .then(snapshot => {
-        snapshot.forEach(function (childSnapshot) {
+        snapshot.forEach(function(childSnapshot) {
           const valor = childSnapshot.val();
           valor.id = childSnapshot.key;
           copyList.push(valor);
@@ -48,7 +49,7 @@ const getCopyForTesting = () => {
       })
 
       .catch(err => {
-        console.log('pics in db didnt download correctly');
+        console.log("pics in db didnt download correctly");
         reject(err);
       });
   });
@@ -56,5 +57,5 @@ const getCopyForTesting = () => {
 
 export default {
   getCopy: getCopy,
-  getCopyForTesting: getCopyForTesting,
+  getCopyForTesting: getCopyForTesting
 };
---
+++ src/utils/FirebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDF1G-uxr_Sg4jHLZAgyuVXsuHnTt8bhzU",
+  authDomain: "resume-40a8a.firebaseapp.com",
+  databaseURL: "https://resume-40a8a.firebaseio.com",
+  projectId: "resume-40a8a",
+  storageBucket: "resume-40a8a.appspot.com",
+  messagingSenderId: "714638486475"
+};
