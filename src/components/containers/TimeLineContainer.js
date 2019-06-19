0008c2f2be383446bba14ff9b00783d2d9123997
ra <ramiro.santamaria.alvarez@gmail.com>
2019-06-19 13:28:41 -0400

    new work , new studies and new job

--- src/components/containers/TimeLineContainer.js
+++ src/components/containers/TimeLineContainer.js
@@ -1,20 +1,19 @@
-
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import actions from '../../actions';
-import {TimeBox} from '../presentational';
-import {TimeLineYears_css, TimeLineActivities_css} from '../../css'; // eslint-disable-line no-unused-vars
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import actions from "../../actions";
+import { TimeBox } from "../presentational";
+import { TimeLineYears_css, TimeLineActivities_css } from "../../css"; // eslint-disable-line no-unused-vars
 
 class TimeLineContainer extends Component {
-  getTimeLineComponents(rawList){
+  getTimeLineComponents(rawList) {
     let timeLineComponentList = [];
-    for(let i = 0; i<rawList.length; i++){
+    for (let i = 0; i < rawList.length; i++) {
       timeLineComponentList.push(
-        <div className = {rawList[i].className} key ={i}>
+        <div className={rawList[i].className} key={i}>
           <TimeBox
-            urlClick = {this.handleClick.bind(this)}
-            copy = {rawList[i]}
-            screenSize = {this.props.section.screenSize}
+            urlClick={this.handleClick.bind(this)}
+            copy={rawList[i]}
+            screenSize={this.props.section.screenSize}
           />
         </div>
       );
@@ -22,53 +21,53 @@ class TimeLineContainer extends Component {
     return timeLineComponentList;
   }
 
-  handleClick(url){
-    if(url !=='falta'){
-      window.open(url,'_blank');
+  handleClick(url) {
+    if (url !== "falta") {
+      window.open(url, "_blank");
     }
   }
   render() {
     let timeLineList = [];
-    if(this.props.copy){
-      if (this.props.copy.timeLineList.length !== 0){
+    if (this.props.copy) {
+      if (this.props.copy.timeLineList.length !== 0) {
         timeLineList = this.getTimeLineComponents(this.props.copy.timeLineList);
       }
     }
-    
-    return (
-      <div className = 'timeLine__container'>
-        {timeLineList}
-        <div className = 'line'>
-        </div>
-        <div className = 'year__container'>
-          <div className = 'eighteen'>2018</div>
-          <div className = 'seventeen'>2017</div>
-          <div className = 'sixteen'>2016</div>
-          <div className = 'fiveteen'>2015</div>
-          <div className = 'twelve'>2012</div>
-          <div className = 'ten'>2010</div>
-          <div className = 'eight'>2008</div>
-          <div className = 'five'>2005</div>
-          <div className = 'zero'>2000</div>
-        </div>
 
+    return (
+      <div className="timeLine__container">
+        {timeLineList}
+        <div className="line" />
+        <div className="year__container">
+          <div className="nineteen">2019</div>
+          <div className="eighteen">2018</div>
+          <div className="seventeen">2017</div>
+          <div className="sixteen">2016</div>
+          <div className="fiveteen">2015</div>
+          <div className="twelve">2012</div>
+          <div className="ten">2010</div>
+          <div className="eight">2008</div>
+          <div className="five">2005</div>
+          <div className="zero">2000</div>
+        </div>
       </div>
     );
   }
 }
 
-const dispatchToProps = (dispatch) =>{
-
-  return{
-    getCopy: () => dispatch(actions.getCopy()),
-
+const dispatchToProps = dispatch => {
+  return {
+    getCopy: () => dispatch(actions.getCopy())
   };
 };
-const stateToProps = (state) => {
-  return{
-    copy :state.copy,
-    section:state.section,
+const stateToProps = state => {
+  return {
+    copy: state.copy,
+    section: state.section
   };
 };
 
-export default connect (stateToProps,dispatchToProps)(TimeLineContainer);
+export default connect(
+  stateToProps,
+  dispatchToProps
+)(TimeLineContainer);
